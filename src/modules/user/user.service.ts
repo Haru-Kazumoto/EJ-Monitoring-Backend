2bc1ac0b07988c27d79cd5e3bb3b5b0b04409d06
Ikhsan892 <jiaathaya@gmail.com>
2023-04-08 17:21:36 +0700

    Update User

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -14,8 +14,8 @@ export class UserService {
   /**
    * Find all records user
    * 
-   * @param userWhereUniqueInput 
-   * @returns unique
+   * @param {userWhereUniqueInput} 
+   * @returns {Promise<user[]>}
    */
   async findUser(userWhereUniqueInput: Prisma.UserWhereUniqueInput): Promise<User | null> {
      return await this.prisma.user.findUnique({
@@ -43,8 +43,8 @@ export class UserService {
   /**
    * Create new 1 record user
    * 
-   * @param data 
-   * @returns user
+   * @param {data} 
+   * @returns {Promise<User>}
    */
   async createUser(data: Prisma.UserCreateInput): Promise<User> {
     try{
@@ -77,8 +77,8 @@ export class UserService {
   /**
    * Updating 1 record user
    * 
-   * @param params 
-   * @returns 
+   * @param {params} 
+   * @returns {Promise<User>}}
    */
   async updateUser(params: {where: Prisma.UserWhereUniqueInput, data: Prisma.UserUpdateInput}): Promise<User> {
     const { where, data } = params;
@@ -88,8 +88,8 @@ export class UserService {
   /**
    * Delete 1 record user
    * 
-   * @param where 
-   * @returns 
+   * @param {where} 
+   * @returns {Promise<User>}
    */
   async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
     let errors = [];
